c6ea7715636e6315750b8994a143ba1d7b667143
Esteban Drouet <Esteban.Drouet@etu.univ-smb.fr>
2025-03-11 09:23:56 +0100

    categorie et accesoire(pas fini)

---
+++ WsRest_UpWay/Models/EntityFramework/Accessoire.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.CodeAnalysis.Elfie.Serialization;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WsRest_UpWay.Models.EntityFramework
+{
+    [Table("t_a_accessoire_acs")]
+
+    public partial class Accessoire
+    {
+        [Key]
+        [Column("acs_id")]
+        public int IdAccessoire { get; set; }
+
+        [Required]
+        [Column("acs_nom")]
+        [StringLength(100)]
+        public string NomAccessoire { get; set; } = null!;
+
+        [Required]
+        [Column("acs_prix")]
+        public decimal PrixAccessoire { get; set; } 
+
+        [Column("acs_description")]
+        [StringLength(1000)]
+        public string DescriptionAccessoire { get; set; }
+
+
+    }
+}
---
+++ WsRest_UpWay/Models/EntityFramework/Categorie.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Identity.Client;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WsRest_UpWay.Models.EntityFramework
+{
+    [Table("t_c_utilisateur_cat")]
+    public partial class Categorie
+    {
+        [Key]
+        [Column("cat_id")]
+        public int IdCategorie { get; set; }
+
+        [Column("cat_libelle")]
+        [StringLength(100)]
+        public string LibelleCategorie { get; set; }
+    }
+}
