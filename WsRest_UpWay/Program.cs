3cca49ff116011ae4b4104aab2a8950988e93871
Esteban Drouet <Esteban.Drouet@etu.univ-smb.fr>
2025-03-12 09:22:06 +0100

    connexion base de données

--- WsRest_UpWay/Program.cs
+++ WsRest_UpWay/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using WsRest_UpWay.Models.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<S215UpWayContext>(options => 
+options.UseNpgsql(builder.Configuration.GetConnectionString("UpwayDBContext")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
