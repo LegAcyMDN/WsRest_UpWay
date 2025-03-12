cc298aee5908ec4c81c5a94bd84d292471c66c3b
Justin Bonansea <Justin.Bonansea@etu.univ-smb.fr>
2025-03-12 16:52:51 +0100

    refactor : model entity name M

--- WsRest_UpWay/Models/EntityFramework/Marquagevelo.cs
+++ WsRest_UpWay/Models/EntityFramework/Marquagevelo.cs
@@ -6,24 +6,24 @@ using Microsoft.EntityFrameworkCore;
 
 namespace WsRest_UpWay.Models.EntityFramework;
 
-[Table("marquagevelo", Schema = "upways")]
+[Table("t_e_marquage_velo_mal", Schema = "upways")]
 [Index("Codemarquage", Name = "codemarquage_unq", IsUnique = true)]
 [Index("Idpanier", "Idvelo", Name = "idx_marquagevelo_idpanier_idvelo")]
 public partial class Marquagevelo
 {
     [Key]
-    [Column("codemarquage")]
+    [Column("mal_code")]
     [StringLength(10)]
     public string Codemarquage { get; set; } = null!;
 
-    [Column("idpanier")]
+    [Column("pan_id")]
     public int Idpanier { get; set; }
 
-    [Column("idvelo")]
+    [Column("vel_id")]
     public int Idvelo { get; set; }
 
-    [Column("prixmarquage")]
-    [Precision(4, 2)]
+    [Column("mal_prix", TypeName = "numeric(2, 2)")]
+    [Precision(2, 2)]
     public decimal? Prixmarquage { get; set; }
 
     [ForeignKey("Idpanier, Idvelo")]
