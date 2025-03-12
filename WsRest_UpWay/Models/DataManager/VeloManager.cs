c333801288fd5bf42806602ffe211455e95a2852
Justin Bonansea <Justin.Bonansea@etu.univ-smb.fr>
2025-03-12 09:32:21 +0100

    ajout VeloManager

---
+++ WsRest_UpWay/Models/DataManager/VeloManager.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WsRest_UpWay.Models.EntityFramework;
+using WsRest_UpWay.Models.Repository;
+
+namespace WsRest_UpWay.Models.DataManager
+{
+    public class VeloManager : IDatarepository<Velo>
+    {
+        public Task AddAsync(Velo entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Velo entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ActionResult<IEnumerable<Velo>>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ActionResult<Velo>> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ActionResult<Velo>> GetByStringAsync(string str)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Velo entityToUpdate, Velo entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
