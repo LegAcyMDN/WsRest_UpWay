9a3150708a44cbd961228cdc0f1590d3fd9a3ca0
Murasaki55 <155960585+Murasaki55@users.noreply.github.com>
2025-03-12 15:22:08 +0100

    DetailCommande annotate

--- WsRest_UpWay/Models/EntityFramework/Detailcommande.cs
+++ WsRest_UpWay/Models/EntityFramework/Detailcommande.cs
@@ -1,39 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace WsRest_UpWay.Models.EntityFramework;
 
+[Table("t_e_detailcommande_detcom")]
 public partial class Detailcommande
 {
-    public int Idcommande { get; set; }
+    [Key]
+    [Column("detcom_id")]
+    public int CommandeId { get; set; }
 
-    public int? Idretraitmagasin { get; set; }
+    [Key]
+    [Column("retmag_id")]
+    public int? RetraitmagasinId { get; set; }
 
-    public int? Idadressefact { get; set; }
+    [Key]
+    [Column("adfact_id")]
+    public int? AdressefactId { get; set; }
 
-    public int? Idetatcommande { get; set; }
+    [Key]
+    [Column("etacom_id")]
+    public int? EtatcommandeId { get; set; }
 
-    public int Idclient { get; set; }
+    [Key]
+    [Column("clt_id")]
+    public int ClientId { get; set; }
 
-    public int? Idpanier { get; set; }
+    [Key]
+    [Column("pan_id")]
+    public int? PanierId { get; set; }
 
+    [Key]
+    [Column("detcom_moypai")]
     public string? Moyenpaiement { get; set; }
 
+    [Key]
+    [Column("detcom_modexp")]
     public string? Modeexpedition { get; set; }
 
+    [Key]
+    [Column("detcom_datacht")]
     public DateOnly? Dateachat { get; set; }
 
-    public virtual Adressefacturation? IdadressefactNavigation { get; set; }
+    [ForeignKey(nameof(AdressefactId))]
+    [InverseProperty(nameof(Adressefacturation.CommandesAdresseFacturation))]
+    public virtual Adressefacturation? AdressefactCommande { get; set; }
 
-    public virtual Compteclient IdclientNavigation { get; set; } = null!;
+    [ForeignKey(nameof(ClientId))]
+    [InverseProperty(nameof(Compteclient.CommandesClient))]
+    public virtual Compteclient ClientCommandant { get; set; } = null!;
 
-    public virtual Etatcommande? IdetatcommandeNavigation { get; set; }
+    [ForeignKey(nameof(EtatcommandeId))]
+    [InverseProperty(nameof(Etatcommande.EtatCommandes))]
+    public virtual Etatcommande? EtatCommande { get; set; }
 
-    public virtual Panier? IdpanierNavigation { get; set; }
+    [ForeignKey(nameof(PanierId))]
+    [InverseProperty(nameof(Panier.CommandesPanier))]
+    public virtual Panier? PanierCommande { get; set; }
 
-    public virtual Retraitmagasin? IdretraitmagasinNavigation { get; set; }
+    [ForeignKey(nameof(RetraitmagasinId))]
+    [InverseProperty(nameof(Retraitmagasin.CommandesRetraitMagasin))]
+    public virtual Retraitmagasin? RetraitmagasinCommande { get; set; }
 
-    public virtual ICollection<Panier> Paniers { get; set; } = new List<Panier>();
-
-    public virtual ICollection<Retraitmagasin> Retraitmagasins { get; set; } = new List<Retraitmagasin>();
+    [InverseProperty(nameof(Panier.CommandePanier))]
+    public virtual ICollection<Panier> PaniersCommande { get; set; } = new List<Panier>();
+    [InverseProperty(nameof(Retraitmagasin.CommandeRetraitmagasins))]
+    public virtual ICollection<Retraitmagasin> RetraitmagasinsCommande { get; set; } = new List<Retraitmagasin>();
 }
