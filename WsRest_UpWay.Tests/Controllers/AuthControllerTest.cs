2a85ed0cabd76256c2f6a023be6a9be092c1b300
RedBoxing <redboxing@redboxing.moe>
2025-03-12 15:33:42 +0100

    Test: Intialize Test Project

---
+++ WsRest_UpWay.Tests/Controllers/AuthControllerTest.cs
@@ -0,0 +1,17 @@
+using JetBrains.Annotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WsRest_UpWay.Controllers;
+
+namespace WsRest_UpWay.Tests.Controllers;
+
+[TestClass]
+[TestSubject(typeof(AuthController))]
+public class AuthControllerTest
+{
+
+    [TestMethod]
+    public void METHOD()
+    {
+        
+    }
+}
