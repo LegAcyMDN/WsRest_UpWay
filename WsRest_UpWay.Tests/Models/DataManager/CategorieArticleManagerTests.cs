75699587058c120b01709c83c7e36deb14b3dcae
Justin Bonansea <Justin.Bonansea@etu.univ-smb.fr>
2025-03-20 10:12:56 +0100

    add : test controller categorie article

---
+++ WsRest_UpWay.Tests/Models/DataManager/CategorieArticleManagerTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WsRest_UpWay.Models.DataManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WsRest_UpWay.Models.DataManager.Tests
+{
+    [TestClass()]
+    public class CategorieArticleManagerTests
+    {
+        [TestMethod()]
+        public void CategorieArticleManagerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CategorieArticleManagerTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetByIdAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetByStringAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateAsyncTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
