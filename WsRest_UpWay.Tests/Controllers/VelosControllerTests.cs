00499f2e7136f3951aceb3b44f6f83d06b165dad
RedBoxing <redboxing@redboxing.moe>
2025-03-25 08:13:05 +0100

    FIX: VeloController tests not compiling

--- WsRest_UpWay.Tests/Controllers/VelosControllerTests.cs
+++ WsRest_UpWay.Tests/Controllers/VelosControllerTests.cs
@@ -1,104 +1,98 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using WsRest_UpWay.Controllers;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using WsRest_UpWay.Models.DataManager;
 using WsRest_UpWay.Models.EntityFramework;
 using WsRest_UpWay.Models.Repository;
-using Microsoft.EntityFrameworkCore;
-using WsRest_UpWay.Models.DataManager;
-using Npgsql;
-using Humanizer;
-using Microsoft.AspNetCore.Routing;
-using NuGet.Protocol.Plugins;
-using System.Composition;
-using Moq;
-using Microsoft.CodeAnalysis.FlowAnalysis;
-using Microsoft.AspNetCore.Mvc;
 
-namespace WsRest_UpWay.Controllers.Tests
+namespace WsRest_UpWay.Controllers.Tests;
+
+[TestClass]
+public class VelosControllerTests
 {
-    [TestClass()]
-    public class VelosControllerTests
+    private IDataVelo data;
+    private S215UpWayContext _context { get; set; }
+    private VelosController _controller { get; set; }
+
+    [TestInitialize]
+    public void Init()
     {
-        private S215UpWayContext _context { get; set; }
-        private VelosController _controller { get; set; }
-        private IDataVelo data;
+        var builder = new DbContextOptionsBuilder<S215UpWayContext>().UseNpgsql("");
+        _context = new S215UpWayContext(builder.Options);
+        data = new VeloManager(_context);
+        _controller = new VelosController(data);
+    }
 
-        [TestInitialize]
-        public void Init() 
+    [TestMethod]
+    public void GetVelosTest()
+    {
+        var result = _controller.GetVelos().Result.Value;
+        var velos = _context.Velos.ToList();
+
+        Assert.IsInstanceOfType(result, typeof(List<Velo>));
+        CollectionAssert.AreEqual(result.ToList(), velos);
+    }
+
+    [TestMethod]
+    public void GetVeloByIdAvecMoq()
+    {
+        var velo = new Velo
         {
-            var builder = new DbContextOptionsBuilder<S215UpWayContext>().UseNpgsql("");
-            _context = new S215UpWayContext(builder.Options);
-            data = new VeloManager(_context);
-            _controller = new VelosController(data);
-        }
-        [TestMethod()]
-        public void GetVelosTest()
+            VeloId = 1,
+            MarqueId = 1,
+            CategorieId = 1,
+            MoteurId = 1,
+            CaracteristiqueVeloId = 1,
+            NomVelo = "Macumba",
+            AnneeVelo = 2021,
+            TailleMin = "1m50",
+            TailleMax = "1m80",
+            PrixRemise = 150,
+            PrixNeuf = 200,
+            PourcentageReduction = 0,
+            DescriptifVelo = "Ce vélo est bien mais en vrai il est pas ouf",
+            QuantiteVelo = 1,
+            PositionMoteur = "Avant",
+            CapaciteBatterie = "120 Wh"
+        };
+        var mockRepository = new Mock<IDataVelo>();
+        mockRepository.Setup(x => x.GetByIdAsync(1).Result).Returns(velo);
+        var velocontroller = new VelosController(mockRepository.Object);
+
+        var actionResult = velocontroller.GetVelo(1).Result;
+    }
+
+    public void GetVeloByFiltresAvecMoq()
+    {
+        var velo = new Velo
         {
-            var result = _controller.GetVelos().Result.Value;
-            List<Velo> velos = _context.Velos.ToList();
+            VeloId = 1,
+            MarqueId = 1,
+            CategorieId = 1,
+            MoteurId = 1,
+            CaracteristiqueVeloId = 1,
+            NomVelo = "Macumba",
+            AnneeVelo = 2021,
+            TailleMin = "150",
+            TailleMax = "180",
+            NombreKms = "347",
+            PrixRemise = 150,
+            PrixNeuf = 200,
+            PourcentageReduction = 0,
+            DescriptifVelo = "Ce vélo est bien mais en vrai il est pas ouf",
+            QuantiteVelo = 1,
+            PositionMoteur = "Avant",
+            CapaciteBatterie = "120 Wh"
+        };
+        var mockRepository = new Mock<IDataVelo>();
+        mockRepository
+            .Setup(x => x.GetByFiltresAsync("150", 1, 1, 1, 2021, "347", "Avant", "Kryptonite", null, "120 Wh", null,
+                null, null, 10)).ReturnsAsync(new ActionResult<IEnumerable<Velo>>(new List<Velo> { velo }));
+        var velocontroller = new VelosController(mockRepository.Object);
 
-            Assert.IsInstanceOfType(result, typeof(List<Velo>));
-            CollectionAssert.AreEqual(result.ToList(), velos);
-        }
-        [TestMethod()]
-        public void GetVeloByIdAvecMoq() 
-        {
-            Velo velo = new Velo
-            {
-                VeloId = 1,
-                MarqueId = 1,
-                CategorieId = 1,
-                MoteurId = 1,
-                CaracteristiqueVeloId = 1,
-                NomVelo = "Macumba",
-                AnneeVelo = 2021,
-                TailleMin = "1m50",
-                TailleMax = "1m80",
-                PrixRemise = 150,
-                PrixNeuf = 200,
-                PourcentageReduction = 0,
-                DescriptifVelo = "Ce vélo est bien mais en vrai il est pas ouf",
-                QuantiteVelo = 1,
-                PositionMoteur = "Avant",
-                CapaciteBatterie = "120 Wh"
-            };
-            var mockRepository = new Mock<IDataVelo>();
-            mockRepository.Setup(x => x.GetByIdAsync(1).Result).Returns(velo);
-            var velocontroller = new VelosController(mockRepository.Object);
-
-            var actionResult = velocontroller.GetVelo(1).Result;
-        }
-        public void GetVeloByFiltresAvecMoq()
-        {
-            Velo velo = new Velo
-            {
-                VeloId = 1,
-                MarqueId = 1,
-                CategorieId = 1,
-                MoteurId = 1,
-                CaracteristiqueVeloId = 1,
-                NomVelo = "Macumba",
-                AnneeVelo = 2021,
-                TailleMin = "150",
-                TailleMax = "180",
-                NombreKms = "347",
-                PrixRemise = 150,
-                PrixNeuf = 200,
-                PourcentageReduction = 0,
-                DescriptifVelo = "Ce vélo est bien mais en vrai il est pas ouf",
-                QuantiteVelo = 1,
-                PositionMoteur = "Avant",
-                CapaciteBatterie = "120 Wh"
-            };
-            var mockRepository = new Mock<IDataVelo>();
-
-            mockRepository.Setup(x => x.GetByFiltresAsync("150", 1, 1, 1, 2021, "347", "Avant", "Kryptonite", null, "120 Wh", null, null, null, 10)).ReturnsAsync(new ActionResult<IEnumerable<Velo>>(new List<Velo> { velo }));
-
-            var actionResult = velocontroller.GetVelo(1).Result;
-        }
+        var actionResult = velocontroller.GetVelo(1).Result;
     }
 }
