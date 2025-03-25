81f55d765a0b058a64262ed62d7939ddc4cf15cb
Justin Bonansea <Justin.Bonansea@etu.univ-smb.fr>
2025-03-25 10:36:57 +0100

    Updated: test MOQ for detailcommande & categoriearticle

--- WsRest_UpWay.Tests/Controllers/CategorieArticlesControllerTests.cs
+++ WsRest_UpWay.Tests/Controllers/CategorieArticlesControllerTests.cs
@@ -17,114 +17,156 @@ namespace WsRest_UpWay.Controllers.Tests
     [TestClass()]
     public class CategorieArticlesControllerTests
     {
-        private CategorieArticlesController _controller;
-        private S215UpWayContext _context;
-        private IDataRepository<CategorieArticle> _dataRepository;
-
-        [TestInitialize]
-        public void Inititialize()
-        {
-            var builder = new DbContextOptionsBuilder<S215UpWayContext>().UseNpgsql("");
-            _context = new S215UpWayContext(builder.Options);
-            _dataRepository = new CategorieArticleManager(_context);
-            _controller = new CategorieArticlesController(_dataRepository);
-        }
-
-        [TestMethod()]
-        public void GetCategoriesArticlesTest()
+        [TestMethod]
+        public void GetCategorieArticleById_ExistingIdPassed_ReturnsRightItem_AvecMoq()
         {
             // Arrange
-            List<CategorieArticle> expected = _context.CategorieArticles.ToList();
-            // Act
-            var res = _controller.GetCategoriesArticles().Result;
-            // Assert
-            CollectionAssert.AreEqual(expected, res.Value.ToList(), "les listes ne sont pas identiques");
-        }
-
-        [TestMethod()]
-        public void GetCategoriesArticleByIdTest_Success()
-        {
-            // Arrange
-            int id = 1;
-            CategorieArticle expected = _context.CategorieArticles.Find(id);
-            // Act
-            var res = _controller.GetCategorieArticleById(1).Result;
-            // Assert
-            Assert.AreEqual(expected, res.Value);
-        }
-
-        [TestMethod()]
-        public void GetCategoriesArticleByIdTest_NotFound()
-        {
-            // Arrange
-            int id = -1;
-            // Act
-            var res = _controller.GetCategorieArticleById(id).Result;
-            // Assert
-            Assert.IsNull(res.Value);
-        }
-
-        [TestMethod()]
-        public void GetCategoriesArticleByTitreTest_Success()
-        {
-            // Arrange
-            String titre = "";
-            CategorieArticle expected = _context.CategorieArticles.FirstOrDefault(u => u.TitreCategorieArticle == titre);
-            // Act
-            var res = _controller.GetCategorieArticleByTitre(titre).Result;
-            // Assert
-            Assert.AreEqual(expected, res.Value);
-        }
-
-        [TestMethod()]
-        public void GetCategoriesArticleByTitreTest_NotFound()
-        {
-            // Arrange
-            String titre = "";
-            // Act
-            var res = _controller.GetCategorieArticleByTitre(titre).Result;
-            // Assert
-            Assert.IsNull(res.Value);
-        }
-
-        [TestMethod()]
-        public void PutCategorieArticleTest()
-        {
-            // Arrange
-            String titre = "";
-            int id = 1;
-            CategorieArticle catArticleATester = _context.CategorieArticles.Find(id);
-            catArticleATester.TitreCategorieArticle = $"{titre}";
-
-            // Act
-            var res = _controller.PutCategorieArticle(id, catArticleATester);
-
-            // Arrange
-            CategorieArticle catArticleMisAJour = _context.CategorieArticles.Find(id);
-            Assert.AreEqual(catArticleATester, catArticleMisAJour);
-        }
-
-        [TestMethod()]
-        public void DeleteEtudiantTest()
-        {
-            // Arrange
-            CategorieArticle catArticleATester = new CategorieArticle
+            CategorieArticle catArticle = new CategorieArticle
             {
-                CategorieArticleId = 100,
-                TitreCategorieArticle = "Méthode d'insertion",
-                ContenuCategorieArticle = "",
-                ImageCategorie = "",
+                CategorieArticleId = 1,
+                TitreCategorieArticle = "Revente vélo",
+                ContenuCategorieArticle = "Toutes les informations et détails à savoir !",
+                ImageCategorie = "nothing.png"
             };
-            _context.CategorieArticles.Add(catArticleATester);
-            _context.SaveChanges();
+            var mockRepository = new Mock<IDataRepository<CategorieArticle>>();
+            mockRepository.Setup(x => x.GetByIdAsync(1).Result).Returns(catArticle);
+            var catArticleController = new CategorieArticlesController(mockRepository.Object);
 
             // Act
-            CategorieArticle deletedcatArticle = _context.CategorieArticles.FirstOrDefault(u => u.TitreCategorieArticle == catArticleATester.TitreCategorieArticle);
-            _ = _controller.DeleteCategorieArticle(deletedcatArticle.CategorieArticleId).Result;
+            var actionResult = catArticleController.GetCategorieArticleById(1).Result;
 
+            // Assert   
+            Assert.IsNotNull(actionResult);
+            Assert.IsNotNull(actionResult.Value);
+            Assert.AreEqual(catArticle, actionResult.Value as CategorieArticle);
+        }
+
+        [TestMethod]
+        public void GetCategorieArticleById_UnknownIdPassed_ReturnsNotFoundResult_AvecMoq()
+        {
+            var mockRepository = new Mock<IDataRepository<CategorieArticle>>();
+            var catArticleController = new CategorieArticlesController(mockRepository.Object);
+
+            // Act
+            var actionResult = catArticleController.GetCategorieArticleById(0).Result;
+
+            // Assert
+            Assert.IsInstanceOfType(actionResult.Result, typeof(NotFoundResult));
+        }
+
+        [TestMethod]
+        public void GetCategorieArticleByTitre_ExistingInePassed_AvecMoq()
+        {
             // Arrange
-            CategorieArticle res = _context.CategorieArticles.FirstOrDefault(u => u.TitreCategorieArticle == catArticleATester.TitreCategorieArticle);
-            Assert.IsNull(res, "etudiant non supprimé");
+            CategorieArticle catArticle = new CategorieArticle
+            {
+                CategorieArticleId = 1,
+                TitreCategorieArticle = "Revente vélo",
+                ContenuCategorieArticle = "Toutes les informations et détails à savoir !",
+                ImageCategorie = "nothing.png"
+            };
+            var mockRepository = new Mock<IDataRepository<CategorieArticle>>();
+            mockRepository.Setup(x => x.GetByStringAsync("Revente vélo").Result).Returns(catArticle);
+            var catArticleController = new CategorieArticlesController(mockRepository.Object);
+
+            // Act
+            var actionResult = catArticleController.GetCategorieArticleByTitre("Revente vélo").Result;
+
+            // Assert
+            Assert.IsNotNull(actionResult);
+            Assert.IsNotNull(actionResult.Value);
+            Assert.AreEqual(catArticle, actionResult.Value as CategorieArticle);
+        }
+
+        [TestMethod]
+        public void GetCategorieArticleByTitre_UnknownInePassed_AvecMoq()
+        {
+            var mockRepository = new Mock<IDataRepository<CategorieArticle>>();
+            var catArticleController = new CategorieArticlesController(mockRepository.Object);
+
+            // Act
+            var actionResult = catArticleController.GetCategorieArticleByTitre("On ne vend pas de switch !").Result;
+
+            // Assert
+            Assert.IsInstanceOfType(actionResult.Result, typeof(NotFoundResult));
+        }
+
+        [TestMethod]
+        public void PutCategorieArticleTest_AvecMoq()
+        {
+            CategorieArticle catArticleToEdit = new CategorieArticle
+            {
+                CategorieArticleId = 1,
+                TitreCategorieArticle = "Revente vélo",
+                ContenuCategorieArticle = "Toutes les informations et détails à savoir !",
+                ImageCategorie = "nothing.png"
+            };
+            // Arrange
+            CategorieArticle catArticleEdited = new CategorieArticle
+            {
+                CategorieArticleId = 1,
+                TitreCategorieArticle = "Revente vélo",
+                ContenuCategorieArticle = "Regarder notre vidéo pour plus d'informations et de détails !",
+                ImageCategorie = "nothing.png"
+            };
+            var mockRepository = new Mock<IDataRepository<CategorieArticle>>();
+            mockRepository.Setup(x => x.GetByIdAsync(1).Result).Returns(catArticleToEdit);
+            var catArticleController = new CategorieArticlesController(mockRepository.Object);
+
+            // Act
+            var actionResult = catArticleController.PutCategorieArticle(1, catArticleEdited).Result;
+
+            // Assert
+            Assert.IsInstanceOfType(actionResult, typeof(NoContentResult), "Pas un NoContentResult");
+        }
+
+        [TestMethod]
+        public void PostCategorieArticleTest_ModelValidated_CreationOK()
+        {
+            // Arrange
+            var mockRepository = new Mock<IDataRepository<CategorieArticle>>();
+            var catArticleController = new CategorieArticlesController(mockRepository.Object);
+            CategorieArticle catArticle = new CategorieArticle
+            {
+                CategorieArticleId = 2,
+                TitreCategorieArticle = "Revente vélo",
+                ContenuCategorieArticle = "Toutes les informations et détails à savoir !",
+                ImageCategorie = "nothing.png"
+            };
+
+            // Act
+            var actionResult = catArticleController.PostCategorieArticle(catArticle).Result;
+
+            // Assert
+            Assert.IsInstanceOfType(actionResult, typeof(ActionResult<CategorieArticle>), "Pas un ActionResult<CategorieArticle>");
+            Assert.IsInstanceOfType(actionResult.Result, typeof(CreatedAtActionResult), "Pas un CreatedAtActionResult");
+            var result = actionResult.Result as CreatedAtActionResult;
+            Assert.IsInstanceOfType(result.Value, typeof(CategorieArticle), "Pas un CategorieArticle");
+            catArticle.CategorieArticleId = ((CategorieArticle)result.Value).CategorieArticleId;
+            Assert.AreEqual(catArticle, (CategorieArticle)result.Value, "CategorieArticles pas identiques");
+        }
+
+
+        [TestMethod]
+        public void DeleteCategorieArticleTest_AvecMoq()
+        {
+            // Arrange
+            CategorieArticle catArticle = new CategorieArticle
+            {
+                CategorieArticleId = 2,
+                TitreCategorieArticle = "Revente vélo",
+                ContenuCategorieArticle = "Regarder notre vidéo pour plus d'informations et de détails !",
+                ImageCategorie = "nothing.png"
+            };
+            var mockRepository = new Mock<IDataRepository<CategorieArticle>>();
+            mockRepository.Setup(x => x.GetByIdAsync(2).Result).Returns(catArticle);
+            var catArticleController = new CategorieArticlesController(mockRepository.Object);
+            
+            // Act
+            var actionResult = catArticleController.DeleteCategorieArticle(2).Result;
+            
+            // Assert
+            Assert.IsInstanceOfType(actionResult, typeof(NoContentResult), "Pas un NoContentResult"); // Test du type de retour
         }
     }
 }
---
+++ WsRest_UpWay.Tests/Controllers/DetailCommandesControllerTests.cs
@@ -0,0 +1,200 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WsRest_UpWay.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using WsRest_UpWay.Models.EntityFramework;
+using WsRest_UpWay.Models.Repository;
+
+namespace WsRest_UpWay.Controllers.Tests
+{
+    [TestClass()]
+    public class DetailCommandesControllerTests
+    {
+        [TestMethod]
+        public void GetDetailCommandeById_ExistingIdPassed_ReturnsRightItem_AvecMoq()
+        {
+            // Arrange
+            DetailCommande detCommande = new DetailCommande
+            {
+                CommandeId = 1,
+                RetraitMagasinId = 1,
+                AdresseFactId = 1,
+                EtatCommandeId = 1,
+                ClientId = 1,
+                PanierId = 1,
+                MoyenPaiement = "Carte Bancaire",
+                ModeExpedition = "Retrait Magasin",
+                DateAchat = DateTime.Now
+            };
+            var mockRepository = new Mock<IDataRepository<DetailCommande>>();
+            mockRepository.Setup(x => x.GetByIdAsync(1).Result).Returns(detCommande);
+            var detCommandeController = new DetailCommandesController(mockRepository.Object);
+
+            // Act
+            var actionResult = detCommandeController.GetDetailCommande(1).Result;
+
+            // Assert   
+            Assert.IsNotNull(actionResult);
+            Assert.IsNotNull(actionResult.Value);
+            Assert.AreEqual(detCommande, actionResult.Value as DetailCommande);
+        }
+
+        [TestMethod]
+        public void GetDetailCommandeById_UnknownIdPassed_ReturnsNotFoundResult_AvecMoq()
+        {
+            var mockRepository = new Mock<IDataRepository<DetailCommande>>();
+            var detCommandeController = new DetailCommandesController(mockRepository.Object);
+
+            // Act
+            var actionResult = detCommandeController.GetDetailCommande(0).Result;
+
+            // Assert
+            Assert.IsInstanceOfType(actionResult.Result, typeof(NotFoundResult));
+        }
+
+        [TestMethod]
+        public void GetDetailCommandeByModeLivraison_ExistingInePassed_AvecMoq()
+        {
+            // Arrange
+            DetailCommande detCommande = new DetailCommande
+            {
+                CommandeId = 1,
+                RetraitMagasinId = 1,
+                AdresseFactId = 1,
+                EtatCommandeId = 1,
+                ClientId = 1,
+                PanierId = 1,
+                MoyenPaiement = "Carte Bancaire",
+                ModeExpedition = "Retrait Magasin",
+                DateAchat = DateTime.Now
+            };
+            var mockRepository = new Mock<IDataRepository<DetailCommande>>();
+            mockRepository.Setup(x => x.GetByStringAsync("Retrait Magasin").Result).Returns(detCommande);
+            var detCommandeController = new DetailCommandesController(mockRepository.Object);
+
+            // Act
+            var actionResult = detCommandeController.GetDetailCommandeByModeLivraison("Retrait Magasin").Result;
+
+            // Assert
+            Assert.IsNotNull(actionResult);
+            Assert.IsNotNull(actionResult.Value);
+            Assert.AreEqual(detCommande, actionResult.Value as DetailCommande);
+        }
+
+        [TestMethod]
+        public void GetDetailCommandeByModeLivraison_UnknownInePassed_AvecMoq()
+        {
+            var mockRepository = new Mock<IDataRepository<DetailCommande>>();
+            var detCommandeController = new DetailCommandesController(mockRepository.Object);
+
+            // Act
+            var actionResult = detCommandeController.GetDetailCommandeByModeLivraison("Marche à pieds").Result;
+
+            // Assert
+            Assert.IsInstanceOfType(actionResult.Result, typeof(NotFoundResult));
+        }
+
+        [TestMethod]
+        public void PutDetailCommandeTest_AvecMoq()
+        {
+            DetailCommande detCommandeToEdit = new DetailCommande
+            {
+                CommandeId = 1,
+                RetraitMagasinId = 1,
+                AdresseFactId = 1,
+                EtatCommandeId = 1,
+                ClientId = 1,
+                PanierId = 1,
+                MoyenPaiement = "Carte Bancaire",
+                ModeExpedition = "Retrait Magasin",
+                DateAchat = DateTime.Now
+            };
+            // Arrange
+            DetailCommande detCommandeEdited = new DetailCommande
+            {
+                CommandeId = 1,
+                RetraitMagasinId = 1,
+                AdresseFactId = 1,
+                EtatCommandeId = 1,
+                ClientId = 1,
+                PanierId = 1,
+                MoyenPaiement = "Apple Pay",
+                ModeExpedition = "Expédition",
+                DateAchat = DateTime.Now
+            };
+            var mockRepository = new Mock<IDataRepository<DetailCommande>>();
+            mockRepository.Setup(x => x.GetByIdAsync(1).Result).Returns(detCommandeToEdit);
+            var detCommandeController = new DetailCommandesController(mockRepository.Object);
+
+            // Act
+            var actionResult = detCommandeController.PutDetailCommande(1, detCommandeEdited).Result;
+
+            // Assert
+            Assert.IsInstanceOfType(actionResult, typeof(NoContentResult), "Pas un NoContentResult");
+        }
+
+        [TestMethod]
+        public void PostDetailCommandeTest_ModelValidated_CreationOK()
+        {
+            // Arrange
+            var mockRepository = new Mock<IDataRepository<DetailCommande>>();
+            var detCommandeController = new DetailCommandesController(mockRepository.Object);
+            DetailCommande detCommande = new DetailCommande
+            {
+                CommandeId = 2,
+                RetraitMagasinId = 15,
+                AdresseFactId = 12,
+                EtatCommandeId = 3,
+                ClientId = 23,
+                PanierId = 24,
+                MoyenPaiement = "Carte Bancaire",
+                ModeExpedition = "Retrait Magasin",
+                DateAchat = DateTime.Now
+            };
+
+            // Act
+            var actionResult = detCommandeController.PostDetailCommande(detCommande).Result;
+
+            // Assert
+            Assert.IsInstanceOfType(actionResult, typeof(ActionResult<DetailCommande>), "Pas un ActionResult<DetailCommande>");
+            Assert.IsInstanceOfType(actionResult.Result, typeof(CreatedAtActionResult), "Pas un CreatedAtActionResult");
+            var result = actionResult.Result as CreatedAtActionResult;
+            Assert.IsInstanceOfType(result.Value, typeof(DetailCommande), "Pas un DetailCommande");
+            detCommande.CommandeId = ((DetailCommande)result.Value).CommandeId;
+            Assert.AreEqual(detCommande, (DetailCommande)result.Value, "DetailCommandes pas identiques");
+        }
+
+
+        [TestMethod]
+        public void DeleteDetailCommandeTest_AvecMoq()
+        {
+            // Arrange
+            DetailCommande detCommande = new DetailCommande
+            {
+                CommandeId = 3,
+                RetraitMagasinId = 1,
+                AdresseFactId = 1,
+                EtatCommandeId = 1,
+                ClientId = 1,
+                PanierId = 1,
+                MoyenPaiement = "Carte Bancaire",
+                ModeExpedition = "Retrait Magasin",
+                DateAchat = DateTime.Now
+            };
+            var mockRepository = new Mock<IDataRepository<DetailCommande>>();
+            mockRepository.Setup(x => x.GetByIdAsync(2).Result).Returns(detCommande);
+            var detCommandeController = new DetailCommandesController(mockRepository.Object);
+
+            // Act
+            var actionResult = detCommandeController.DeleteDetailCommande(2).Result;
+
+            // Assert
+            Assert.IsInstanceOfType(actionResult, typeof(NoContentResult), "Pas un NoContentResult"); // Test du type de retour
+        }
+    }
+}
