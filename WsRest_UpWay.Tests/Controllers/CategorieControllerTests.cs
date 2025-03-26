977f9e9aa1aadbd5c32fedb5272788c4450a49d3
hugoclercrenaud <Hugo.Clerc-Renaud@etu.univ-smb.fr>
2025-03-26 16:15:35 +0100

    add : CategorieController teste

---
+++ WsRest_UpWay.Tests/Controllers/CategorieControllerTests.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WsRest_UpWay.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WsRest_UpWay.Models.EntityFramework;
+using WsRest_UpWay.Models.Repository;
+using Microsoft.EntityFrameworkCore;
+using WsRest_UpWay.Models.DataManager;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace WsRest_UpWay.Controllers.Tests
+{
+    [TestClass()]
+    public class CategoriesControllerTests
+    {
+        [TestMethod]
+        public void GetCategorieById_ExistingIdPassed_ReturnsRightItem_AvecMoq()
+        {
+            // Arrange
+            Categorie cat = new Categorie
+            {
+                CategorieId = 1,
+                LibelleCategorie = "nothing.png"
+            };
+            var mockRepository = new Mock<IDataRepository<Categorie>>();
+            mockRepository.Setup(x => x.GetByIdAsync(1).Result).Returns(cat);
+            var catController = new CategoriesController(mockRepository.Object);
+
+            // Act
+            var actionResult = catController.GetCategorieById(1).Result;
+
+            // Assert   
+            Assert.IsNotNull(actionResult);
+            Assert.IsNotNull(actionResult.Value);
+            Assert.AreEqual(cat, actionResult.Value as Categorie);
+        }
+
+        [TestMethod]
+        public void GetCategorieById_UnknownIdPassed_ReturnsNotFoundResult_AvecMoq()
+        {
+            var mockRepository = new Mock<IDataRepository<Categorie>>();
+            var catController = new CategoriesController(mockRepository.Object);
+
+            // Act
+            var actionResult = catController.GetCategorieById(0).Result;
+
+            // Assert
+            Assert.IsInstanceOfType(actionResult.Result, typeof(NotFoundResult));
+        }
+
+        [TestMethod]
+        public void GetCategorieByTitre_ExistingTitrePassed_AvecMoq()
+        {
+            // Arrange
+            Categorie cat = new Categorie
+            {
+                CategorieId = 1,
+                LibelleCategorie = "nothing.png"
+            };
+            var mockRepository = new Mock<IDataRepository<Categorie>>();
+            mockRepository.Setup(x => x.GetByStringAsync("Revente vélo").Result).Returns(cat);
+            var catController = new CategoriesController(mockRepository.Object);
+
+            // Act
+            var actionResult = catController.GetCategorieByTitre("Revente vélo").Result;
+
+            // Assert
+            Assert.IsNotNull(actionResult);
+            Assert.IsNotNull(actionResult.Value);
+            Assert.AreEqual(cat, actionResult.Value as Categorie);
+        }
+
+        [TestMethod]
+        public void GetCategorieByTitre_UnknownTitrePassed_AvecMoq()
+        {
+            var mockRepository = new Mock<IDataRepository<Categorie>>();
+            var catController = new CategoriesController(mockRepository.Object);
+
+            // Act
+            var actionResult = catController.GetCategorieByTitre("On ne vend pas de switch !").Result;
+
+            // Assert
+            Assert.IsInstanceOfType(actionResult.Result, typeof(NotFoundResult));
+        }
+
+        [TestMethod]
+        public void PutCategorieTest_AvecMoq()
+        {
+            Categorie catToEdit = new Categorie
+            {
+                CategorieId = 1,
+                LibelleCategorie = "nothing.png"
+            };
+            // Arrange
+            Categorie catEdited = new Categorie
+            {
+                CategorieId = 1,
+                LibelleCategorie = "nothing.png"
+            };
+            var mockRepository = new Mock<IDataRepository<Categorie>>();
+            mockRepository.Setup(x => x.GetByIdAsync(1).Result).Returns(catToEdit);
+            var catController = new CategoriesController(mockRepository.Object);
+
+            // Act
+            var actionResult = catController.PutCategorie(1, catEdited).Result;
+
+            // Assert
+            Assert.IsInstanceOfType(actionResult, typeof(NoContentResult), "Pas un NoContentResult");
+        }
+
+        [TestMethod]
+        public void PostCategorieTest_ModelValidated_CreationOK()
+        {
+            // Arrange
+            var mockRepository = new Mock<IDataRepository<Categorie>>();
+            var catController = new CategoriesController(mockRepository.Object);
+            Categorie cat = new Categorie
+            {
+                CategorieId = 2,
+                LibelleCategorie = "nothing.png"
+            };
+
+            // Act
+            var actionResult = catController.PostCategorie(cat).Result;
+
+            // Assert
+            Assert.IsInstanceOfType(actionResult, typeof(ActionResult<Categorie>), "Pas un ActionResult<Categorie>");
+            Assert.IsInstanceOfType(actionResult.Result, typeof(CreatedAtActionResult), "Pas un CreatedAtActionResult");
+            var result = actionResult.Result as CreatedAtActionResult;
+            Assert.IsInstanceOfType(result.Value, typeof(Categorie), "Pas un Categorie");
+            cat.CategorieId = ((Categorie)result.Value).CategorieId;
+            Assert.AreEqual(cat, (Categorie)result.Value, "Categories pas identiques");
+        }
+
+
+        [TestMethod]
+        public void DeleteCategorieTest_AvecMoq()
+        {
+            // Arrange
+            Categorie cat = new Categorie
+            {
+                CategorieId = 2,
+                LibelleCategorie = "nothing.png"
+            };
+            var mockRepository = new Mock<IDataRepository<Categorie>>();
+            mockRepository.Setup(x => x.GetByIdAsync(2).Result).Returns(cat);
+            var catController = new CategoriesController(mockRepository.Object);
+
+            // Act
+            var actionResult = catController.DeleteCategorie(2).Result;
+
+            // Assert
+            Assert.IsInstanceOfType(actionResult, typeof(NoContentResult), "Pas un NoContentResult"); // Test du type de retour
+        }
+    }
+}
---
+++ WsRest_UpWay.Tests/Models/DataManager/CategorieManagerTests.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WsRest_UpWay.Models.DataManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using WsRest_UpWay.Models.EntityFramework;
+
+namespace WsRest_UpWay.Models.DataManager.Tests
+{
+    [TestClass()]
+    [TestSubject(typeof(CategorieManager))]
+    public class CategorieManagerTests
+    {
+        private S215UpWayContext ctx;
+        private CategorieManager manager;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var builder = new DbContextOptionsBuilder<S215UpWayContext>();
+            builder.UseSqlite("Data Source=S215UpWay.db");
+
+            ctx = new S215UpWayContext(builder.Options);
+            ctx.Database.Migrate();
+
+            manager = new CategorieManager(ctx);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            ctx.Database.EnsureDeleted();
+        }
+
+        [TestMethod]
+        public void GetAllAsyncTest()
+        {
+            var result = manager.GetAllAsync().Result;
+
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Value);
+            CollectionAssert.AreEquivalent(ctx.Categories.ToList(), result.Value.ToList());
+        }
+
+        [TestMethod]
+        public void GetByIdAsyncTest()
+        {
+            var expected = ctx.Categories.FirstOrDefault();
+            if (expected == null)
+                return; // db is either empty or not working, stop the test without error to avoid false negative when db is empty
+
+            var result = manager.GetByIdAsync(expected.CategorieId).Result;
+
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Value);
+            Assert.AreEqual(expected, result.Value);
+        }
+
+        [TestMethod]
+        public void AddAsyncTest()
+        {
+            var store = new Categorie
+            {
+                LibelleCategorie = "Toutes les informations et détails à savoir !"
+            };
+
+            manager.AddAsync(store).Wait();
+
+            var store2 = ctx.Categories.First(u => u.LibelleCategorie == store.LibelleCategorie);
+            Assert.IsNotNull(store2);
+
+            ctx.Categories.Remove(store2);
+            ctx.SaveChanges();
+        }
+
+        [TestMethod]
+        public void UpdateAsyncTest()
+        {
+            var store = ctx.Categories.FirstOrDefault();
+            if (store == null) return; // we can't test if the db is empty
+
+            var orig = store.LibelleCategorie;
+            var newP = "Achat de vélo";
+            store.LibelleCategorie = newP;
+
+            manager.UpdateAsync(store, store).Wait();
+
+            store = ctx.Categories.Find(store.CategorieId);
+            Assert.IsNotNull(store);
+            Assert.AreEqual(newP, store.LibelleCategorie);
+
+            store.LibelleCategorie = orig;
+            manager.UpdateAsync(store, store).Wait();
+        }
+
+        [TestMethod]
+        public void RemoveAsyncTest()
+        {
+            var store = new Categorie
+            {
+                LibelleCategorie = "Toutes les informations et détails à savoir !"
+            };
+
+            store = ctx.Categories.Add(store).Entity;
+            ctx.SaveChanges();
+            Assert.IsNotNull(store);
+
+            manager.DeleteAsync(store).Wait();
+            store = ctx.Categories.Find(store.CategorieId);
+            Assert.IsNull(store);
+        }
+
+    }
+}
