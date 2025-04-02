54911ca0ba3134573f11b33f0ab2dc8266a5fb6f
Justin Bonansea <Justin.Bonansea@etu.univ-smb.fr>
2025-04-02 17:47:52 +0200

    Updated: create Manager for letter P

---
+++ WsRest_UpWay/Models/DataManager/PhotoAccessoireManager.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Drawing.Printing;
+using WsRest_UpWay.Models.EntityFramework;
+using WsRest_UpWay.Models.Repository;
+
+namespace WsRest_UpWay.Models.DataManager
+{
+    public class PhotoAccessoireManager : IDataRepository<PhotoAccessoire>
+    {
+        public const int PAGE_SIZE = 20;
+        private readonly S215UpWayContext upwaysDbContext;
+
+        public PhotoAccessoireManager()
+        {
+        }
+
+        public PhotoAccessoireManager(S215UpWayContext context)
+        {
+            upwaysDbContext = context;
+        }
+
+        public async Task AddAsync(PhotoAccessoire phoAcs)
+        {
+            await upwaysDbContext.Photoaccessoires.AddAsync(phoAcs);
+            await upwaysDbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(PhotoAccessoire phoAcs)
+        {
+            upwaysDbContext.Photoaccessoires.Remove(phoAcs);
+            await upwaysDbContext.SaveChangesAsync();
+        }
+
+        public async Task<ActionResult<IEnumerable<PhotoAccessoire>>> GetAllAsync(int page = 0)
+        {
+            return await upwaysDbContext.Photoaccessoires.Skip(page * PAGE_SIZE).Take(PAGE_SIZE).ToListAsync();
+        }
+
+        public async Task<ActionResult<PhotoAccessoire>> GetByIdAsync(int id)
+        {
+            return await upwaysDbContext.Photoaccessoires.FirstOrDefaultAsync(p => p.PhotoAcessoireId == id);
+        }
+
+        public async Task<ActionResult<PhotoAccessoire>> GetByStringAsync(string str)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ActionResult<int>> GetCountAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateAsync(PhotoAccessoire phoAcsToUpdate, PhotoAccessoire phoAcs)
+        {
+            upwaysDbContext.Entry(phoAcsToUpdate).State = EntityState.Modified;
+            phoAcsToUpdate.PhotoAcessoireId = phoAcs.PhotoAcessoireId;
+            phoAcsToUpdate.AccessoireId = phoAcs.AccessoireId;
+            phoAcsToUpdate.UrlPhotoAccessoire = phoAcs.UrlPhotoAccessoire;
+            await upwaysDbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ WsRest_UpWay/Models/DataManager/PhotoVeloManager.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WsRest_UpWay.Models.EntityFramework;
+using WsRest_UpWay.Models.Repository;
+
+namespace WsRest_UpWay.Models.DataManager
+{
+    public class PhotoVeloManager : IDataRepository<PhotoVelo>
+    {
+        public const int PAGE_SIZE = 20;
+        private readonly S215UpWayContext upwaysDbContext;
+
+        public PhotoVeloManager()
+        {
+        }
+
+        public PhotoVeloManager(S215UpWayContext context)
+        {
+            upwaysDbContext = context;
+        }
+
+        public async Task AddAsync(PhotoVelo phoVel)
+        {
+            await upwaysDbContext.Photovelos.AddAsync(phoVel);
+            await upwaysDbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(PhotoVelo phoVel)
+        {
+            upwaysDbContext.Photovelos.Remove(phoVel);
+            await upwaysDbContext.SaveChangesAsync();
+        }
+
+        public async Task<ActionResult<IEnumerable<PhotoVelo>>> GetAllAsync(int page = 0)
+        {
+            return await upwaysDbContext.Photovelos.Skip(page * PAGE_SIZE).Take(PAGE_SIZE).ToListAsync();
+        }
+
+        public async Task<ActionResult<PhotoVelo>> GetByIdAsync(int id)
+        {
+            return await upwaysDbContext.Photovelos.FirstOrDefaultAsync(p => p.PhotoVeloId == id);
+        }
+
+        public async Task<ActionResult<PhotoVelo>> GetByStringAsync(string str)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ActionResult<int>> GetCountAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateAsync(PhotoVelo phoVelToUpdate, PhotoVelo phoVel)
+        {
+            upwaysDbContext.Entry(phoVelToUpdate).State = EntityState.Modified;
+            phoVelToUpdate.PhotoVeloId = phoVel.PhotoVeloId;
+            phoVelToUpdate.VeloId = phoVel.VeloId;
+            phoVelToUpdate.UrlPhotoVelo = phoVel.UrlPhotoVelo;
+            phoVelToUpdate.PhotoBytea = phoVel.PhotoBytea;
+            await upwaysDbContext.SaveChangesAsync();
+        }
+    }
+}
