bec44016f7fa00ced3f8b21604cc58eac9f43330
Justin Bonansea <Justin.Bonansea@etu.univ-smb.fr>
2025-04-09 15:25:18 +0200

    Uodated: test moq for CodeReduction

--- WsRest_UpWay.Tests/Controllers/CodeReductionsControllerTests.cs
+++ WsRest_UpWay.Tests/Controllers/CodeReductionsControllerTests.cs
@@ -1,33 +1,29 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using WsRest_UpWay.Controllers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using WsRest_UpWay.Models.EntityFramework;
 using WsRest_UpWay.Models.Repository;
 
 namespace WsRest_UpWay.Controllers.Tests
 {
-[TestClass()]
-public class CodeReductionsControllerTests
-{
-    private CodeReductionsController _controller;
+    [TestClass()]
+    public class CodeReductionsControllerTests
+    {
+        private CodeReductionsController _controller;
         private Mock<IDataRepository<CodeReduction>> _mockDataRepository;
 
-    [TestInitialize]
-    public void TestInitialize()
-    {
+        [TestInitialize]
+        public void TestInitialize()
+        {
             _mockDataRepository = new Mock<IDataRepository<CodeReduction>>();
             _controller = new CodeReductionsController(_mockDataRepository.Object);
-    }
+        }
 
-    [TestMethod]
+        [TestMethod]
         public async Task GetCodeReduction_ReturnsOkResult_WhenCodeReductionExist()
-    {
+        {
             // Arrange
             var codeReduction = new List<CodeReduction>
             {
@@ -43,11 +39,11 @@ public class CodeReductionsControllerTests
             var returnedCodeReduction = result.Value as List<CodeReduction>;
             Assert.IsNotNull(returnedCodeReduction);
             CollectionAssert.AreEquivalent(codeReduction, returnedCodeReduction);
-    }
+        }
 
-    [TestMethod]
+        [TestMethod]
         public async Task GetCodeReductionById_ExistingIdPassed_ReturnsRightItem_AvecMoq()
-    {
+        {
             // Arrange
             var codeReductionId = "1";
             var codeReduction = new CodeReduction { ReductionId = "1", ActifReduction = true, };
@@ -60,23 +56,23 @@ public class CodeReductionsControllerTests
             var returnedCodeReduction = result.Value;
             Assert.IsNotNull(returnedCodeReduction);
             Assert.AreEqual(codeReductionId, returnedCodeReduction.ReductionId);
-    }
+        }
 
-    [TestMethod]
+        [TestMethod]
         public async Task GetCodeReduction_ReturnsNotFound_WhenCodeReductionDoesNotExist()
-    {
+        {
             var codeReductionId = "1";
             _mockDataRepository.Setup(repo => repo.GetByStringAsync(codeReductionId)).ReturnsAsync((CodeReduction)null);
 
             var result = await _controller.GetCodeReduction("J7S8Zd9");
 
             // Assert
-        Assert.IsInstanceOfType(result.Result, typeof(NotFoundResult));
-    }
+            Assert.IsInstanceOfType(result.Result, typeof(NotFoundResult));
+        }
 
-    [TestMethod]
+        [TestMethod]
         public async Task PostCodeReduction_ReturnsCreatedResult_WhenModelIsValid()
-    {
+        {
             // Arrange
             // Arrange
             var newcodeReduction = new CodeReduction
@@ -94,11 +90,11 @@ public class CodeReductionsControllerTests
             Assert.IsNotNull(createdAtActionResult);
             Assert.AreEqual("GetCodeReduction", createdAtActionResult.ActionName);
             Assert.AreEqual(newcodeReduction.ReductionId, createdAtActionResult.RouteValues["id"]);
-    }
+        }
 
-    [TestMethod]
+        [TestMethod]
         public async Task PostCodeReduction_ReturnsBadRequest_WhenModelIsInvalid()
-    {
+        {
             // Arrange
             var newCodeReduction = new CodeReduction();
             _controller.ModelState.AddModelError("ActifReduction", "Required");
@@ -109,15 +105,15 @@ public class CodeReductionsControllerTests
             // Assert
             var badRequestResult = result.Result as BadRequestObjectResult;
             Assert.IsNotNull(badRequestResult);
-    }
+        }
 
-    [TestMethod]
+        [TestMethod]
         public async Task PutCodeReduction_ReturnsNoContent_WhenCodeReductionIsUpdated()
         {
             // Arrange
             var codeReductionId = "1";
             var updatedCodeReduction = new CodeReduction
-    {
+            {
                 ReductionId = "1",
                 ActifReduction = true,
             };
@@ -130,15 +126,15 @@ public class CodeReductionsControllerTests
 
             // Assert
             Assert.IsInstanceOfType(result, typeof(NoContentResult));
-    }
+        }
 
-    [TestMethod]
+        [TestMethod]
         public async Task PutCodeReduction_ReturnsNotFound_WhenCodeReductionDoesNotExist()
         {
             // Arrange
             var codeReductionId = "1";
             var updatedCodeReduction = new CodeReduction
-    {
+            {
                 ReductionId = "1",
                 ActifReduction = true,
             };
@@ -149,15 +145,15 @@ public class CodeReductionsControllerTests
 
             // Assert
             Assert.IsInstanceOfType(result, typeof(NotFoundResult));
-    }
+        }
 
-    [TestMethod]
+        [TestMethod]
         public async Task DeleteCodeReduction_ReturnsNoContent_WhenCodeReductionIsDeleted()
         {
             // Arrange
             var codeReductionId = "1";
             var CodeReduction = new CodeReduction
-    {
+            {
                 ReductionId = "1",
                 ActifReduction = true,
             };
@@ -168,12 +164,12 @@ public class CodeReductionsControllerTests
             var result = await _controller.DeleteCodeReduction(codeReductionId);
 
             // Assert
-        Assert.IsInstanceOfType(result, typeof(NoContentResult));
-    }
+            Assert.IsInstanceOfType(result, typeof(NoContentResult));
+        }
 
-    [TestMethod]
+        [TestMethod]
         public async Task DeleteCodeReduction_ReturnsNotFound_WhenCodeReductionDoesNotExist()
-    {
+        {
             // Arrange
             var codeReductionId = "1";
             _mockDataRepository.Setup(repo => repo.GetByStringAsync(codeReductionId)).ReturnsAsync((CodeReduction)null);
@@ -182,7 +178,7 @@ public class CodeReductionsControllerTests
             var result = await _controller.DeleteCodeReduction("D78I6Z");
 
             // Assert
-        Assert.IsInstanceOfType(result, typeof(NotFoundResult));
+            Assert.IsInstanceOfType(result, typeof(NotFoundResult));
+        }
     }
-}
 }
